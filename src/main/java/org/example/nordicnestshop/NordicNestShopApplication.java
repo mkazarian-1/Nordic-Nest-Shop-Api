83b63e256b50c7978e3ed84d7af5a221926d1874
Mykhailo Kazarian <misha.kazaryan2@gmail.com>
2025-02-03 12:50:13 +0200

    Initial commit

---
+++ src/main/java/org/example/nordicnestshop/NordicNestShopApplication.java
@@ -0,0 +1,13 @@
+package org.example.nordicnestshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NordicNestShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NordicNestShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/nordicnestshop/NordicNestShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nordicnestshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NordicNestShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
