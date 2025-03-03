d19a74c4b6558be5ccc27f0679c9d96e070a86f4
Mykhailo Kazarian <misha.kazaryan2@gmail.com>
2025-03-03 16:14:37 +0200

    Security implementation (Hot fix2)

--- src/main/java/org/example/nordicnestshop/repository/UserRepository.java
+++ src/main/java/org/example/nordicnestshop/repository/UserRepository.java
@@ -1,12 +1,8 @@
 package org.example.nordicnestshop.repository;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import org.example.nordicnestshop.model.User;
-import org.example.nordicnestshop.model.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findUserByEmail(String email);
